a04d56540eb910f02a777651147d9d5255cb1cc2
Adrian <adrian.burgos.colas@gmail.com>
2025-10-19 19:40:23 -0400

    add home and components pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,42 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Design System Docs',
+      customCss: [
+        // Path to your Tailwind base styles:
+        './src/styles/global.css',
+      ],
+      social: [
+        { icon: 'x.com', label: 'X', href: 'https://x.com' },
+        { icon: 'facebook', label: 'Facebook', href: 'https://facebook.com' },
+        { icon: 'instagram', label: 'Instagram', href: 'https://instagram.com' },
+      ],
+      sidebar: [
+        {
+          label: 'Components',
+          items: [
+            { label: 'Overview', slug: 'components/overview' },
+            { label: 'Ation bar', slug: 'components/actionbar' },
+            { label: 'Banner', slug: 'components/banner' },
+          ],
+        },
+        {
+          label: 'Patterns',
+          items: [
+            { label: 'Overview', slug: 'patterns/overview' },
+          ],
+        },
+      ],
+    }),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
